8e0dd622346852fb675b563fc6697076e0ea6575
mirsanova <kseniya.solopina@gmail.com>
2015-08-16 12:08:21 +0300

    add second project

---
+++ README.md
@@ -0,0 +1,7 @@
+> Как установить мой проект
+
+1. git clone https://github.com/mirsanova/loftschool_dz2.git
+2. cd loftschool_dz2
+3. bower i
+4. npm i
+5. gulp
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    plumber = require('gulp-plumber'),
+    jade = require('gulp-jade');
+    // spritesmith = require('gulp.spritesmith');
+
+gulp.task('server', function () {
+  browserSync({
+    port: 9000,
+    server: {
+      baseDir: 'app'
+    }
+  });
+});
+//Компиляция шаблона
+gulp.task('jade', function() {
+   gulp.src('app/templates/pages/*.jade')
+    .pipe(plumber())
+    .pipe(jade({pretty: true}))
+    .pipe(gulp.dest('app'))
+    .pipe(browserSync.reload({stream:true}))
+    .on('error',console.log)
+    .pipe(plumber.stop());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['app/templates/pages/*.jade'], ['jade']);
+  gulp.watch([
+    'app/*.html',
+    'app/js/**/*.js',
+    'app/css/**/*.css'
+  ]).on('change', browserSync.reload);
+});
+
+// gulp.task('sprite', function() {
+//     var spriteData =
+//         gulp.src('app/sprite/*.*') // путь, откуда берем картинки для спрайта
+//             .pipe(spritesmith({
+//                 imgName: 'sprite.png',
+//                 cssName: 'sprite.css',
+//             }));
+
+//     spriteData.img.pipe(gulp.dest('app/img/')); // путь, куда сохраняем картинку
+//     spriteData.css.pipe(gulp.dest('app/css/')); // путь, куда сохраняем стили
+// });
+
+gulp.task('default', ['server', 'watch']);
